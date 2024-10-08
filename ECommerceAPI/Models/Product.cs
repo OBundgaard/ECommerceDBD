7653f2dfe2b1582f10d6578828fc48704df4350a
Oliver <Oliverbundgaard@live.dk>
2024-10-08 11:41:12 +0200

    Initial setup of EF and models for the project

---
+++ ECommerceAPI/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceAPI.Models;
+
+public class Product
+{
+    [Key]
+    public int ProductID { get; set; }
+
+    [Required]
+    public string? Name { get; set; }
+
+    [Required]
+    public decimal Price { get; set; }
+}
